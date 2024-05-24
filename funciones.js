04b87573311b9e3617650743b10edfc17d89fee0
edilop <eddie.lopez.3755@gmail.com>
2024-05-24 13:39:16 -0500

    Segundo commit

--- funciones.js
+++ funciones.js
@@ -1,14 +1,17 @@
-function doGet() {
+function doGet()
+{
   return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda AppScript');
 }
 
 
-function obtenerDatosHtml(nombre){
+function obtenerDatosHtml(nombre)
+{
   return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
 
-function obtenerContactos(){
-  let hoja = SpreadsheetApp.openById('1GXzNAh_lA8yckJ2gHrwl6ODqTwc6k-wmczpLnie_rdM/').getActiveSheet();
+function obtenerContactos()
+{
+  let hoja = SpreadsheetApp.openById('1GXzNAh_lA8yckJ2gHrwl6ODqTwc6k-wmczpLnie_rdM').getActiveSheet();
   let datos = hoja.getDataRange().getValues();
   return datos;
 }
